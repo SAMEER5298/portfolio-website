c84711817b1b31fd3766ff111c8f8c848fe95399
SAMEER5298 <sameerkhan86316@gmail.com>
2026-04-08 09:39:03 +0530

    fix: cors vercel url added

--- portfolio/server/index.ts
+++ portfolio/server/index.ts
@@ -9,7 +9,9 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'https://portfolio-website-8vxp.vercel.app',
+  ],
   credentials: true,
 }));
 app.use(express.json());
